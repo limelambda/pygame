f38814b1e1bc652de42040059486cefc23d4da73
Ford Stewart <25stewartfo11@daretolearn.org>
2021-12-10 20:30:12 -0500

    First real commit smooth mouvment using arrow keys

--- main.py
+++ main.py
@@ -3,11 +3,41 @@ import pygame
 import time
 
 def main():
+
+    def load_sprite(img,size):
+        return pygame.transform.scale(pygame.image.load(img), (size, size))
+
     class Entity:
-        def __init__(self, x, y, sprite):
+        def __init__(self, x, y, sprite, x_speed = 0, y_speed = 0, direction = "down"):
+            print(self.__class__.__name__, "created!")
+            self.x_speed = x_speed
+            self.y_speed = y_speed
             self.x = x
             self.y = y
             self.sprite = sprite
+            self.direction = direction
+        
+        def do_move(self):
+            self.x += self.x_speed / 2
+            self.y += self.y_speed / 2
+            self.x_speed = round(self.x_speed/1.1, 4)
+            self.y_speed = round(self.y_speed/1.1, 4)
+
+        def do_blit(self):
+            print(self.x_speed, self.y_speed)
+            if abs(self.x_speed) != abs(self.y_speed):
+                if abs(self.x_speed) > abs(self.y_speed):
+                    if self.x_speed < 0:
+                        self.direction = "left"
+                    else:
+                        self.direction = "right"
+                else:
+                    if self.y_speed < 0:
+                        self.direction = "up"
+                    else:
+                        self.direction = "down"
+            screen.blit(player.sprite[self.direction], (player.x,player.y))
+
     # initializing the pygame module
     pygame.init()
     #load icon
@@ -18,11 +48,13 @@ def main():
     running = True
      
     #make player
-    player = Entity(128, 128, pygame.transform.scale(pygame.image.load("player.png"), (64, 64)))
+    player = Entity(128, 128, {
+        "down":load_sprite("player/down.png",64), 
+        "up":load_sprite("player/up.png",64), 
+        "left":load_sprite("player/left.png",64), 
+        "right":load_sprite("player/right.png",64)})
     while running:
         screen_width, screen_height = pygame.display.get_surface().get_size()
-        screen.blit(player.sprite, (player.x,player.y))
-        print(screen_width, screen_height)
         #handle events 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -31,14 +63,17 @@ def main():
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_ESCAPE:
                     running = False
-                elif event.key == pygame.K_UP:
-                    player.y -= 10
-                elif event.key == pygame.K_DOWN:
-                    player.y += 10
-                elif event.key == pygame.K_RIGHT:
-                    player.x += 10
-                elif event.key == pygame.K_LEFT:
-                    player.x -= 10
+        pressed = pygame.key.get_pressed()
+        if pressed[pygame.K_UP]:
+            player.y_speed -= 1
+        if pressed[pygame.K_DOWN]:
+            player.y_speed += 1
+        if pressed[pygame.K_RIGHT]:
+            player.x_speed += 1
+        if pressed[pygame.K_LEFT]:
+            player.x_speed -= 1
+        player.do_move()
+        player.do_blit()
         #render
         pygame.display.flip()
         screen.fill((200,200,0))
