9ee68dd6fb28ddd97b721728a70630a972d76fc7
Ford <25stewartfo11@daretolearn.org>
2021-12-11 20:11:03 -0500

    Added music + static colliders

--- main.py
+++ main.py
@@ -4,28 +4,58 @@ import time
 
 def main():
 
-    def load_sprite(img,size):
+    def load_sprite(img, size = 64):
         return pygame.transform.scale(pygame.image.load(img), (size, size))
+    
+    def colliding(x_1,y_1,x_2,y_2):
+        #range(x_1, x_1 + 64) in range(x_2, x_2 + 64) and range(y_1, y_1 + 64) in range(y_2, y_2 + 64)
+        x_range = range(x_2, x_2 + 64)
+        y_range = range(y_2, y_2 + 64)
+        if x_1 in x_range or x_1 + 64 in x_range:
+            return y_1 in y_range or y_1 + 64 in y_range
+
+    class Collidable:
+        def __init__(self, x, y, sprite):
+            #print("wall created!")
+            self.x = x
+            self.y = y
+            self.sprite = load_sprite(sprite) 
+
+        def do_blit(self):
+            screen.blit(self.sprite,(self.x,self.y))
 
     class Entity:
-        def __init__(self, x, y, sprite, x_speed = 0, y_speed = 0, direction = "down"):
-            print(self.__class__.__name__, "created!")
+        def __init__(self, x, y, sprites, x_speed = 0, y_speed = 0, direction = "down"):
             self.x_speed = x_speed
             self.y_speed = y_speed
             self.x = x
             self.y = y
-            self.sprite = sprite
+            self.sprites = sprites
             self.direction = direction
         
         def do_move(self):
-            self.x += self.x_speed / 2
-            self.y += self.y_speed / 2
+            global lvl_elements
+            prev_x = self.x
+            prev_y = self.y
+            self.x = int(round(self.x + self.x_speed / 2))
+            self.y = int(round(self.y + self.y_speed / 2))
+            for i in lvl_elements:
+                if colliding(self.x, self.y, i.x, i.y):
+                    print(f"at coordinates {self.x, self.y} collision occoured at speed {self.x_speed, self.y_speed}")
+                    self.x = prev_x
+                    self.y = prev_y
+                    self.x_speed = 0
+                    self.y_speed = 0
+                    print(f"Position now {self.x, self.y}")
             self.x_speed = round(self.x_speed/1.1, 4)
             self.y_speed = round(self.y_speed/1.1, 4)
 
         def do_blit(self):
-            print(self.x_speed, self.y_speed)
-            if abs(self.x_speed) != abs(self.y_speed):
+            if abs(self.x_speed) < 0.0006:
+                self.x_speed = 0
+            if abs(self.y_speed) < 0.0006:
+                self.y_speed = 0
+            if self.x_speed != self.y_speed:
                 if abs(self.x_speed) > abs(self.y_speed):
                     if self.x_speed < 0:
                         self.direction = "left"
@@ -36,29 +66,40 @@ def main():
                         self.direction = "up"
                     else:
                         self.direction = "down"
-            screen.blit(player.sprite[self.direction], (player.x,player.y))
+            screen.blit(load_sprite(self.sprites[self.direction]), (self.x,self.y))
 
-    # initializing the pygame module
+    def blit_lvl_1():
+        global lvl_elements
+        _100_100 = Collidable(200,200,"assets/level_assets/wall_s.png")
+        lvl_elements = [_100_100]
+        for i in lvl_elements:
+            i.do_blit()
+    #initializing the pygame module
     pygame.init()
     #load icon
     pygame.display.set_caption("Gam")
-    # creating 240 x 180 screen surface
-    screen = pygame.display.set_mode((540, 265), pygame.RESIZABLE)
-    # define a variable to control the main loop
+    #creating 240 x 180 screen surface
+    screen = pygame.display.set_mode((1280, 720), pygame.SCALED)
+    #define a variable to control the main loop
     running = True
-     
+    #do da audio
+    pygame.mixer.init()
+    pygame.mixer.music.load("whomp.wav")
     #make player
     player = Entity(128, 128, {
-        "down":load_sprite("player/down.png",64), 
-        "up":load_sprite("player/up.png",64), 
-        "left":load_sprite("player/left.png",64), 
-        "right":load_sprite("player/right.png",64)})
+        "down":"assets/player/down.png", 
+        "up":"assets/player/up.png", 
+        "left":"assets/player/left.png", 
+        "right":"assets/player/right.png"})
     while running:
+        blit_lvl_1()
+        if not pygame.mixer.music.get_busy():
+            pygame.mixer.music.play()
         screen_width, screen_height = pygame.display.get_surface().get_size()
         #handle events 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                # change the value to False, to exit the main loop
+                #change the value to False, to exit the main loop
                 running = False
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_ESCAPE:
@@ -77,7 +118,6 @@ def main():
         #render
         pygame.display.flip()
         screen.fill((200,200,0))
-
         #fps limiter
         time.sleep(0.01)
 
