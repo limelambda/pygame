244b34856fe092b68022b1dad3a21b177bf32ba8
Ford <25stewartfo11@daretolearn.org>
2021-12-12 21:46:46 -0500

    Items added!

--- main.py
+++ main.py
@@ -4,25 +4,57 @@ import time
 
 def main():
 
+    #making things just so I can nonlocal them
+    lvl_elements = None
+
+    #making game variables
+    inventory = []
+
     def load_sprite(img, size = 64):
         return pygame.transform.scale(pygame.image.load(img), (size, size))
     
     def colliding(x_1,y_1,x_2,y_2):
-        #range(x_1, x_1 + 64) in range(x_2, x_2 + 64) and range(y_1, y_1 + 64) in range(y_2, y_2 + 64)
         x_range = range(x_2, x_2 + 64)
         y_range = range(y_2, y_2 + 64)
         if x_1 in x_range or x_1 + 64 in x_range:
             return y_1 in y_range or y_1 + 64 in y_range
 
-    class Collidable:
+    def blit_lvl_1():
+        nonlocal lvl_elements
+        _0_256 = Basic_lvl_element(0,256,"assets/level_assets/wall_s.png")
+        _64_256 = Basic_lvl_element(64,256,"assets/level_assets/wall_s.png")
+        _128_256 = Basic_lvl_element(128,256,"assets/level_assets/wall_s.png")
+        _192_256 = Basic_lvl_element(192,256,"assets/level_assets/wall_s.png")
+        _96_320 = Item_lvl_element(96,320,"assets/level_assets/heal.png","health potion")
+        lvl_elements = [_0_256,_64_256,_128_256,_192_256,_96_320]
+
+
+    class Basic_lvl_element:
         def __init__(self, x, y, sprite):
-            #print("wall created!")
+            print(f"Basic lvl element crated using sprite {sprite}!")
             self.x = x
             self.y = y
             self.sprite = load_sprite(sprite) 
 
         def do_blit(self):
             screen.blit(self.sprite,(self.x,self.y))
+
+    class Item_lvl_element:
+        def __init__(self, x, y, sprite, items):
+            print(f"Item lvl element crated using sprite {sprite}!")
+            self.x = x
+            self.y = y
+            self.sprite = load_sprite(sprite)
+            self.items = items
+
+        def do_blit(self):
+            screen.blit(self.sprite,(self.x,self.y))
+
+        def on_collision(self):
+            nonlocal inventory, lvl_elements
+            inventory.append(self.items)
+            print(f"Player picked up items:{self.items}")
+            lvl_elements.remove(self)
 
     class Entity:
         def __init__(self, x, y, sprites, x_speed = 0, y_speed = 0, direction = "down"):
@@ -32,9 +64,10 @@ def main():
             self.y = y
             self.sprites = sprites
             self.direction = direction
+            print(f"Entity crated using sprites {sprites}!")
         
         def do_move(self):
-            global lvl_elements
+            nonlocal lvl_elements
             prev_x = self.x
             prev_y = self.y
             self.x = int(round(self.x + self.x_speed / 2))
@@ -42,6 +75,12 @@ def main():
             for i in lvl_elements:
                 if colliding(self.x, self.y, i.x, i.y):
                     print(f"at coordinates {self.x, self.y} collision occoured at speed {self.x_speed, self.y_speed}")
+                    try:
+                        i.on_collision() 
+                    except:
+                        print(f"no speical collision funstion triggered for {i.__class__.__name__}")
+                    else:
+                        print(f"speical collision funstion triggered for {i.__class__.__name__}")
                     self.x = prev_x
                     self.y = prev_y
                     self.x_speed = 0
@@ -68,12 +107,6 @@ def main():
                         self.direction = "down"
             screen.blit(load_sprite(self.sprites[self.direction]), (self.x,self.y))
 
-    def blit_lvl_1():
-        global lvl_elements
-        _100_100 = Collidable(200,200,"assets/level_assets/wall_s.png")
-        lvl_elements = [_100_100]
-        for i in lvl_elements:
-            i.do_blit()
     #initializing the pygame module
     pygame.init()
     #load icon
@@ -84,15 +117,17 @@ def main():
     running = True
     #do da audio
     pygame.mixer.init()
-    pygame.mixer.music.load("whomp.wav")
+    pygame.mixer.music.load("assets/song.mp3")
     #make player
     player = Entity(128, 128, {
         "down":"assets/player/down.png", 
         "up":"assets/player/up.png", 
         "left":"assets/player/left.png", 
         "right":"assets/player/right.png"})
+    blit_lvl_1()
     while running:
-        blit_lvl_1()
+        for i in lvl_elements:
+            i.do_blit()
         if not pygame.mixer.music.get_busy():
             pygame.mixer.music.play()
         screen_width, screen_height = pygame.display.get_surface().get_size()
