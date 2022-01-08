40a68b85cc0541132464ff22da951e14780599e7
Ford <25stewartfo11@daretolearn.org>
2022-01-07 22:29:09 -0500

    battle room > nicer collision > fixed health bar > moved to bottom

--- main.py
+++ main.py
@@ -6,15 +6,17 @@ def main():
     #making game variables
     lvl_elements = None
     background = None
+    grass_background = pygame.image.load("assets/grass.png")
 
     #define functions
-    load_sprite = lambda img: pygame.transform.scale(pygame.image.load(img), (80, 80))
+    load_sprite = lambda img, x = 80, y = 80: pygame.transform.scale(pygame.image.load(img), (x, y))
    
     def colliding(x_1,y_1,x_2,y_2):
         x_range = range(x_2 + 1, x_2 + 80)
         y_range = range(y_2 + 1, y_2 + 80)
-        if x_1 in x_range or x_1 + 80 in x_range:
-            return y_1 in y_range or y_1 + 80 in y_range
+        if x_1 + 1 in x_range or x_1 + 80 in x_range:
+            return y_1 + 1 in y_range or y_1 + 80 in y_range
+        return x_1 < 0 or x_1 > 1280 or y_1 < 0 or y_1 > 720
  
     class Basic_lvl_element:
         def __init__(self, x, y, sprite):
@@ -76,8 +78,8 @@ def main():
         def draw_health(self):
             tuple_subtraction = lambda a,b:tuple(map(lambda a,b:a-b, a, b))
             self.health_anim = (self.health_anim + self.health) / 2
-            pygame.draw.rect(screen, ((200,100,100)), pygame.Rect(20, 20, 1240, 40))
-            pygame.draw.rect(screen, ((100,200,100)), pygame.Rect(20, 20, 1240-(100-self.health_anim)*12.8, 40))
+            pygame.draw.rect(screen, ((200,100,100)), pygame.Rect(20, 660, 1240, 40))
+            pygame.draw.rect(screen, ((100,200,100)), pygame.Rect(20, 660, 1240-(100-self.health_anim)*12.8, 40))
  
         def do_move(self):
             nonlocal lvl_elements
@@ -92,7 +94,6 @@ def main():
                         i.on_collision()
                         prev_x, prev_y = (self.x, self.y)
                     except AttributeError:
-#                        print(f"no speical collision function triggered for {i.__class__.__name__}")
                         pass
                     else:
                         print(f"speical collision function triggered for {i.__class__.__name__}")
@@ -100,7 +101,6 @@ def main():
                     self.y = prev_y
                     self.x_speed = 0
                     self.y_speed = 0
-#                    print(f"Position now {self.x, self.y}")
             self.x_speed = round(self.x_speed/1.1, 4)
             self.y_speed = round(self.y_speed/1.1, 4)
  
@@ -137,27 +137,36 @@ def main():
                 self.health -= 25
  
     def load_battle():
-        nonlocal lvl_elements, player
+        nonlocal lvl_elements, player, background
+        background = lambda : screen.fill((0,0,0))
         player.x = 640
-        player.y = 360
+        player.y = 480
         wall_sprite = load_sprite("assets/level_assets/wall.png")
         wall = lambda x,y:Basic_lvl_element(x*80,y*80,wall_sprite)
         lvl_elements = [
-        wall(8,3),
-        wall(8,6),
-        wall(9,3),
-        wall(9,4),
-        wall(9,5),
-        wall(9,6),
         wall(7,3),
-        wall(7,4),
-        wall(7,5),
-        wall(7,6),
+        wall(7,7),
+        wall(8,3),
+        wall(8,7),
+        wall(9,3),
+        wall(9,7),
+        wall(10,3),
+        wall(10,4),
+        wall(10,5),
+        wall(10,6),
+        wall(10,7),
+        wall(6,3),
+        wall(6,4),
+        wall(6,5),
+        wall(6,6),
+        wall(6,7),
+        #boundry
+        Interactable_lvl_element(640,320,load_sprite("assets/weapons/katana.png", 16, 80),lambda : player.damage(20))
         ]
- 
+
     def load_lvl_1():
         nonlocal lvl_elements, player, background
-        background = pygame.image.load("assets/grass.png")
+        background = lambda : screen.blit(grass_background, (0, 0))
         wall_sprite = load_sprite("assets/level_assets/wall.png")
         heart_sprite = load_sprite("assets/level_assets/heal.png")
         wall = lambda x,y:Basic_lvl_element(x*80,y*80,wall_sprite)
@@ -170,6 +179,7 @@ def main():
         wall(4,4),
         wall(2,5),
         wall(4,5),
+        wall(4,6),
         wall(15,4),
         Interactable_lvl_element(80,400,heart_sprite,player.heal),
         Interactable_lvl_element(0,160,heart_sprite,player.heal),
@@ -198,7 +208,7 @@ def main():
     load_lvl_1()
     while running:
         #blit level
-        screen.blit(background, (0, 0))
+        background()
         for i in lvl_elements:
             i.do_blit()
         if music:
@@ -227,8 +237,7 @@ def main():
         pygame.display.flip()
         #fps limiter
         pygame.time.Clock().tick(45)
-        print((int(pygame.mouse.get_pos()[0]/80),int(pygame.mouse.get_pos()[1]/80)))
- 
+
 if __name__=="__main__":
     #call the main function
     main()
